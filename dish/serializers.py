70faa03448d1640c6ee4e40ab873b40e11f81fd6
Sai <you@example.com>
2021-04-26 21:56:24 +0600

    add dish update and create

--- dish/serializers.py
+++ dish/serializers.py
@@ -5,3 +5,19 @@ class DishListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
         fields = ('id', 'name', 'price')
+
+class DishCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dish
+        fields = ('name', 'price')
+
+
+class DishUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dish
+        fields = ('name', 'price')
+
+class DishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dish
+        fields = ('id', 'name', 'price')
